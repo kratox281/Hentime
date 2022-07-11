518a49493116ada0168bacfe506414909cc106d0
ruben <rcmartinez281@gmail.com>
2022-07-11 16:28:45 +0200

    v1

---
+++ ProyectoHentime/src/main/java/com/hentime/main/ProyectoHentimeApplication.java
@@ -0,0 +1,13 @@
+package com.hentime.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHentimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHentimeApplication.class, args);
+	}
+
+}
---
+++ ProyectoHentime/src/test/java/com/hentime/main/ProyectoHentimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hentime.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHentimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
